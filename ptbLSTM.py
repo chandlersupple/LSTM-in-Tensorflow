c1f1765f04457beaf3952219b28415e480cf6b8c
Chandler Supple <36590015+chandlersupple@users.noreply.github.com>
2018-08-31 19:05:04 -0700

    Update ptbLSTM.py

--- ptbLSTM.py
+++ ptbLSTM.py
@@ -3,8 +3,8 @@
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
-with open('ptb.train.txt') as ptbtextfile: # We'll train our LSTM network on the PTB dataset
-    ptbtext = ptbtextfile.readlines()
+with open('ptb.train.txt') as sptextfile: # We'll train our LSTM network on all Shakespeare's published plays
+    sptext = sptextfile.readlines()
     
 def numerize(text): # Represents words with values, where each unique word is assigned a unique value
     chars = []
@@ -20,17 +20,19 @@ def numerize(text): # Represents words with values, where each unique word is as
             new_text.append(char_dict.get(text[line][char]))
     return new_text, len(chars), chars, char_dict
         
-ntext, vocab_size, chars, char_dict = numerize(ptbtext) # Applies the 'numerize' function to our data
+text, vocab_size, chars, char_dict = numerize(sptext) # Applies the 'numerize' function to our data
 
-class ptbLSTM():
-    def __init__(self, ntext, vocab_size, batch_size, time_steps, hidden_units, num_layers):
+class spLSTM():
+    
+    def __init__(self, text, vocab_size, batch_size, time_steps, hidden_units, num_layers, mode):
         # Parameters
         hidden_units = hidden_units 
         num_layers = num_layers
         self.batch_size = batch_size
         self.time_steps = time_steps
-        self.ntext = ntext
+        self.text = text
         self.vocab_size = vocab_size
+        self.mode = mode
         
         # Containers for the input and target data
         self.x = tf.placeholder(tf.int32, [self.batch_size, self.time_steps])
@@ -38,8 +40,9 @@ class ptbLSTM():
         
         with tf.variable_scope('main_scope', reuse= tf.AUTO_REUSE):
             # Building the LSTM layers
-            lstm_unit = tf.contrib.rnn.BasicLSTMCell(hidden_units)
-            stacked_lstm = tf.contrib.rnn.MultiRNNCell([lstm_unit] * num_layers)
+            lstm_layers = [tf.contrib.rnn.BasicLSTMCell(hidden_units)] * num_layers
+            dropout_lstm = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob= 0.5) for lstm in lstm_layers]
+            stacked_lstm = tf.contrib.rnn.MultiRNNCell(dropout_lstm)
             
             init_state = stacked_lstm.zero_state(self.batch_size, tf.float32) # Memory initial state
             init = tf.random_uniform_initializer(-0.1, 0.1) # Variable initializer
@@ -50,7 +53,7 @@ class ptbLSTM():
             output, new_state = tf.nn.dynamic_rnn(stacked_lstm, ebx, initial_state= init_state)
             output = tf.reshape(output, [-1, hidden_units])
             
-            # Fully connected layer
+                # Fully connected layer
             W1 = tf.get_variable('W1', [hidden_units, self.vocab_size], initializer= init)
             b1 = tf.get_variable('b1', [self.vocab_size], initializer= init)
             
@@ -69,10 +72,10 @@ class ptbLSTM():
             self.train_op = optimizer.apply_gradients(zip(grads, tvars))
         
     def accuracy(self, lx, ly): # Calculates the accuracy of the model
-        soft_out = tf.nn.softmax(tf.reshape(sess.run(ptblstm.logits, {ptblstm.x: lx, ptblstm.y: ly}), [-1, ptblstm.vocab_size]))
+        soft_out = tf.nn.softmax(tf.reshape(sess.run(splstm.logits, {splstm.x: lx, splstm.y: ly}), [-1, splstm.vocab_size]))
         so_pred = tf.cast(tf.argmax(soft_out, 1), tf.int32)
-        correct_preds = tf.equal(so_pred, tf.reshape(ly, [-1]))
-        return sess.run(tf.reduce_mean(tf.cast(correct_preds, tf.float32)))
+        correct = tf.equal(so_pred, tf.reshape(ly, [-1]))
+        return sess.run(tf.reduce_mean(tf.cast(correct, tf.float32)))
     
     def train(self, sess, feed_dict):
         return sess.run(self.train_op, feed_dict= feed_dict)
@@ -82,40 +85,40 @@ class ptbLSTM():
         batchy = []
         for bb in range (self.batch_size):
             batch_marker = (batch_iter * self.time_steps * self.batch_size) + (bb * self.time_steps)
-            batchx.append(self.ntext[batch_marker:batch_marker+self.time_steps])
-            batchy.append(self.ntext[batch_marker+1:batch_marker+self.time_steps+1])
+            batchx.append(self.text[batch_marker:batch_marker+self.time_steps])
+            batchy.append(self.text[batch_marker+1:batch_marker+self.time_steps+1])
         return batchx, batchy
     
-def sample(seed, sess, num_chars, time_steps, chars, char_dict): # The seed must have the same number of characters as there are time steps
-    stext = []
-    num_seed = []
+def sample(sess, words, time_steps, seed, chars, char_dict):
+    _text = []
     seed = list(seed)
+    num_seed = []
     for char in range (len(seed)):
         num_seed.append(char_dict.get(seed[char]))
-    predictions = sess.run(sample_ptb.logits, {sample_ptb.x: [num_seed] * sample_ptb.batch_size})
+    predictions = sess.run(sample_rnn.logits, {sample_rnn.x: [num_seed] * sample_rnn.batch_size})
     pred_am = sess.run(tf.argmax(predictions, axis= 1))
     for ts in range (time_steps):
-        stext.append(chars[pred_am[ts]])
-    for char in range (num_chars):
-        predictions = sess.run(sample_ptb.logits, {sample_ptb.x: [pred_am] * sample_ptb.batch_size})
+        _text.append(chars[pred_am[ts]])
+    for word in range (words):
+        predictions = sess.run(sample_rnn.logits, {sample_rnn.x: [pred_am] * sample_rnn.batch_size})
         pred_am = sess.run(tf.argmax(predictions, axis= 1))
-        stext.append(chars[pred_am[-1]])
-    return stext
+        _text.append(chars[pred_am[-1]])
+    return _text
         
-ptblstm = ptbLSTM(ntext, vocab_size, 16, 32, 256, 2) # Creates an instance of our 'spLSTM' class
-sample_ptb = ptbLSTM(ntext, vocab_size, 1, 32, 256, 2) # Creates an instance with a single batch for sampling
+splstm = spLSTM(text, vocab_size, 16, 32, 256, 2, 1) # Creates an instance of our 'spLSTM' class
+sample_rnn = spLSTM(text, vocab_size, 1, 32, 256, 2, 1)
 
-batches_in_epoch = len(ntext) // (ptblstm.batch_size * ptblstm.time_steps + 1) # Determines the number of batches in each epoch
+batches_in_epoch = len(text) // (splstm.batch_size * splstm.time_steps + 1)
 sess.run(tf.global_variables_initializer()) # Initializes variables
 
 # Training
-for epoch_iter in range (32): # Tge number of epochs the network will perform
+for epoch_iter in range (32):
     for batch_iter in range (batches_in_epoch):
-        lx, ly = ptblstm.build_batch(batch_iter, batches_in_epoch)
-        ptblstm.train(sess, {ptblstm.x: lx, ptblstm.y: ly})
+        lx, ly = splstm.build_batch(batch_iter, batches_in_epoch)
+        splstm.train(sess, {splstm.x: lx, splstm.y: ly})
         if batch_iter % 200 == 0:
-            cost = sess.run(ptblstm.cost, {ptblstm.x: lx, ptblstm.y: ly})
-            acc = ptblstm.accuracy(lx, ly)
+            cost = sess.run(splstm.cost, {splstm.x: lx, splstm.y: ly})
+            acc = splstm.accuracy(lx, ly)
             print('Cost: %s, Accuracy: %s, %s / %s Batches, Epoch %s' %(cost, acc, batch_iter, batches_in_epoch, epoch_iter))
             p_bar = []
             for p_batch in range (1, batches_in_epoch, (batches_in_epoch // 100)):
@@ -125,4 +128,4 @@ for epoch_iter in range (32): # Tge number of epochs the network will perform
                     p_bar.append('>')
             print(''.join(p_bar))                
             print('\n')
-    print(''.join(sample('This statement will seed the net', sess, 100, 32, chars, char_dict))) # Samples text from our network at the end of each epoch
+    print(''.join(sample(sess, 100, 32, 'i believe that he had done a rat', chars, char_dict)))
